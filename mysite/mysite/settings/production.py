0c49dff2466ae994f01ca69f0901cd7442178669
agbo <agboemmanuel002@gmail.com>
2020-10-06 14:27:45 +0100

    scar

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -20,13 +20,28 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asbq1(f!nruh)gxk!mcz_%%p9x*m4fxgo32njbd*!+ugri0b(1'
+SECRET_KEY = os.environ.get('SECRET_KEY','asbq1(f!nruh)gxk!mcz_%%p9x*m4fxgo32njbd*!+ugri0b(1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['olaprep1.herokuapp.com']
 
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='agboemmanuel002@gmail.com'
+EMAIL_HOST_PASSWORD= os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT=587
+EMAIL_USER_TLS=True
+DEFAULT_FROM_EMAIL='agboemmanuel002@gmail.com'
+DEFAULT_TO_EMAIL='maron15686@unknmail.com'
+
+
+ADMINS = (
+       ('agboemmanuel002@gmail.com'),
+    )
+MANAGERS = ADMINS
 
 # Application definition
 
@@ -84,7 +99,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# add this
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
